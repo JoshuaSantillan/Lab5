8951f79829ab5c5a7d41eea3aebdc017b4844c48
Joshua  Santillan <joshuasantillan@Joshuas-MacBook-Pro-2.local>
2021-05-01 18:12:35 -0700

    fix for now

--- script.js
+++ script.js
@@ -1,22 +1,154 @@
 // script.js
 
 const img = new Image(); // used to load image from <input> and draw to canvas
+//============================
+//Select Elements
+//============================
+let getMeme = document.getElementById("generate-meme");
+let genButton = document.getElementsByTagName("button")[0];
+let clrButton = document.getElementsByTagName("button")[1];
+let readButton = document.getElementsByTagName("button")[2];
+let loadImage = document.getElementById("image-input");
+let voiceVolume = document.getElementById("input")[3];
+let canvas = document.getElementById('user-image');
+let ctx = canvas.getContext('2d');
+
+//============
+// State Variables
+// ===========
+let lang;
+let vol = 1.0;
+let getVoices = [];
+
+//====================
+// Speech API Speech Synthesis Helper Functions
+//====================
+speechSynthesis.addEventListener("voiceschanged", () => {
+    getVoices = speechSynthesis.getVoices();
+    let voiceSelection = document.getElementById("voice-selection");
+    voiceSelection.innerHTML="";
+    let i = 0;
+    while(i < getVoices.length){
+      let optionSelect = document.createElement('option');
+      optionSelect.textContent = getVoices[i].name + ' (' +  getVoices[i].lang + ')';
+      // for default case
+      if(getVoices[i].default)
+        optionSelect.textContent += ' -- DEFAULT';
+
+      // set the langauge and name and append to DOM Tree
+      optionSelect.setAttribute('data-lang', getVoices[i].lang);
+      optionSelect.setAttribute('data-name', getVoices[i].name);
+      voiceSelection.appendChild(optionSelect);
+      i += 1;
+    }
+
+    // Set a default value for langauge 
+    voiceSelection.disabled = false;
+    lang = getVoices[0]; 
+    //Change from default langauge:
+    voiceSelection.addEventListener("change", () =>{
+      let selectLangauge = getVoices.selectedOptions[0].getAttribute('data-name');
+      let i = 0;
+      while(i < getVoices.length){
+        if(selectLangauge === getVoices[i].name)
+            lang = getVoices[i]
+        i++;
+      }
+    });
+    
+}); // END SpeechSynthesis
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  const canvas = document.getElementById('user-image');
-  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0,0,400,400); // given size
+  let topText = document.getElementById('text-top');
+  let bottomText = document.getElementById('text-bottom');
+  // set texts to empty
+  topText.value="";
+  bottomText.value="";
+   //Change button state
+  clrButton.disabled = true;
+  readButton.disabled = true;
+  genButton.disabled = false;
+  
+  // Cover cnavas with black then set the img. 
+  ctx.fillStyle="black";
+  ctx.fillRect(0,0,400,400);
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0,0,canvas.width, canvas.height);
-  // TODO
+  let imageDimension = getDimmensions(400,400,img.width,img.height);
+  ctx.drawImage(img,imageDimension.startX,imageDimension.startY,imageDimension.width, imageDimension.height);
 
-  // Some helpful tips:
-  // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
-  // - Clear the form when a new image is selected
-  // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
 
+//Load Image EventListener
+loadImage.addEventListener('change', ()=> {
+  let path = URL.createObjectURL(loadImage.files[0]);
+  let splice = path.split("\\");
+  img.src = path;
+  img.alt = splice[splice.length-1];  
+});
+
+// MemeText Event Listener
+getMeme.addEventListener('submit', (event) =>{
+  //if the event does not get explicitly handled, its default action should not be taken as it normally would be.
+  event.preventDefault();
+  let topText = document.getElementById('text-top');
+  let bottomText = document.getElementById('text-bottom');
+
+  //Font style for meme
+  ctx.font = "30px impact";
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.textAllign = "center";
+  ctx.fillText(bottomText.value,200,385);
+  ctx.fillText(topText.value,200,35);
+  ctx.strokeText(bottomText.value,200,385);
+  ctx.strokeText(topText.value,200,35);
+
+  // now enable them
+ clrButton.disabled = false; 
+ readButton.disabled = false;
+
+})
+
+//Clear the Canvas now using clear button Event Listener
+clrButton.addEventListener('click',()=>{
+  let topText = document.getElementById('text-top');
+  topText.value="";
+  let bottomText = document.getElementById('text-bottom');
+  bottomText.value="";
+  ctx.clearRect(0,0,canvas.width,canvas.height);  
+})
+
+/*
+// text to speech Event Listener
+readButton.addEventListener('click', () => {
+  let topText = documents.getElementById('text-top');
+  let bottomText = document.getElementById('text-top');
+  let tts  = new SpeechSynthesisUtterance(topText.value + " , " + bottomText.value);
+  tts.voice = lang;
+  tts.volume = lang;
+});
+*/
+/*
+//Change volume event listener
+voiceVolume.addEventListener("input", () => {
+  let voiceImage = document.getElementsByTagName('img')[0]; // no volume
+  if(voiceVolume.value == 0){
+    voiceImage.src="icons/volume-level-0.svg";
+    voiceImage.alt="Volume Level 0: 0";
+  }else if(voiceVolume.value>=1 && voiceVolume.value<=33){ // low volume
+    voiceImage.src="icons/volume-level-1.svg";
+    voiceImage.alt="Volume Level 1: 1-33";
+  }else if(voiceVolume.value>=34 && voiceVolume.value<=66){ // medium volume
+    voiceImage.src="icons/volume-level-2.svg";
+    voiceImage.alt="Volume Level 2: 34-66";
+  }else if(voiceVolume.value>=67 && voiceVolume.value<=100){ // high volume 
+    voiceImage.src="icons/volume-level-3.svg";
+    voiceImage.alt="Volume Level 3: 67-100";
+  }
+});
+*/
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
  * dimensions of the image so that it fits perfectly into the Canvas and maintains aspect ratio
